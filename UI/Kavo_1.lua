2085e393e7c735cdcd834c71222c25f3e4de362d
NIX <163978576+bitef4@users.noreply.github.com>
2024-06-06 02:37:09 -0400

    Color Change

--- UI/Kavo_1.lua
+++ UI/Kavo_1.lua
@@ -70,8 +70,8 @@ local themeStyles = {
         TextColor = Color3.fromRGB(0,0,0),
         ElementColor = Color3.fromRGB(224, 224, 224)
     },
-    BloodTheme = {
-        SchemeColor = Color3.fromRGB(227, 27, 27),
+    BlueTheme = {
+        SchemeColor = Color3.fromRGB(26, 41, 255),
         Background = Color3.fromRGB(10, 10, 10),
         Header = Color3.fromRGB(5, 5, 5),
         TextColor = Color3.fromRGB(255,255,255),
@@ -155,8 +155,8 @@ function Kavo.CreateLib(kavName, themeList)
         themeList = themeStyles.DarkTheme
     elseif themeList == "LightTheme" then
         themeList = themeStyles.LightTheme
-    elseif themeList == "BloodTheme" then
-        themeList = themeStyles.BloodTheme
+    elseif themeList == "BlueTheme" then
+        themeList = themeStyles.BlueTheme
     elseif themeList == "GrapeTheme" then
         themeList = themeStyles.GrapeTheme
     elseif themeList == "Ocean" then
